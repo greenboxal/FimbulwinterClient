aff093bff4df54e4d86f300292648cff7a651390
Gabriel Soldani <gabriel.soldani@gmail.com>
2012-07-29 21:28:29 -0300

    forgot the new file

---
+++ FimbulwinterClient/FimbulwinterClient/GUI/System/FPSCounter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace FimbulwinterClient.GUI.System
+{
+    public class FPSCounter : DrawableGameComponent
+    {
+        SpriteBatch spriteBatch;
+        SpriteFont spriteFont;
+
+        int frameRate = 0;
+        int frameCounter = 0;
+        TimeSpan elapsedTime = TimeSpan.Zero;
+
+        private ROClient _client;
+
+        public FPSCounter(ROClient roc) : base(roc)
+        {
+            _client = roc;
+            
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            spriteBatch = new SpriteBatch(_client.GraphicsDevice);
+            spriteFont = ROClient.Singleton.GuiManager.Client.Content.Load<SpriteFont>("fb\\Gulim8b"); ;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            elapsedTime += gameTime.ElapsedGameTime;
+
+            if (elapsedTime > TimeSpan.FromSeconds(1))
+            {
+                elapsedTime -= TimeSpan.FromSeconds(1);
+                frameRate = frameCounter;
+                frameCounter = 0;
+            }
+        }
+
+
+        public override void Draw(GameTime gameTime)
+        {
+            frameCounter++;
+
+            string fps = string.Format("fps: {0}", frameRate);
+
+            spriteBatch.Begin();
+
+            spriteBatch.DrawString(spriteFont, fps, new Vector2(33, 33), Color.Black);
+            spriteBatch.DrawString(spriteFont, fps, new Vector2(32, 32), Color.White);
+
+            spriteBatch.End();
+        }
+    }
+}
