cdda695f193576d1b8833a87cf400a6614bd7d94
Jonathan Lima <greenboxal@gmail.com>
2012-08-10 23:09:43 -0300

    Added shaders wrappers.

---
+++ FimbulvetrEngine/FimbulvetrEngine/Graphics/Shader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK.Graphics.OpenGL;
+
+namespace FimbulvetrEngine.Graphics
+{
+    public class Shader
+    {
+        public int Id { get; private set; }
+        public ShaderType Type { get; private set; }
+        public int Lenght { get; private set; }
+
+        public Shader(ShaderType type, string code)
+        {
+            int len = 0;
+
+            Id = GL.CreateShader(type);
+            Type = type;
+
+            GL.ShaderSource(Id, 1, new[] { code }, ref len);
+            GL.CompileShader(Id);
+
+            int success;
+            GL.GetShader(Id, ShaderParameter.CompileStatus, out success);
+
+            if (success == 0)
+            {
+                int logLen;
+                GL.GetShader(Id, ShaderParameter.CompileStatus, out logLen);
+
+                if (logLen > 0)
+                {
+                    throw new Exception("Error loading shader:\n" + GL.GetShaderInfoLog(Id));
+                }
+            }
+
+            Lenght = len;
+        }
+
+        ~Shader()
+        {
+            GL.DeleteShader(Id);
+        }
+    }
+}
---
+++ FimbulvetrEngine/FimbulvetrEngine/Graphics/ShaderProgram.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FimbulvetrEngine.Content;
+using OpenTK.Graphics.OpenGL;
+
+namespace FimbulvetrEngine.Graphics
+{
+    public class ShaderProgram
+    {
+        public int Id { get; private set; }
+        public List<Shader> Shaders { get; private set; }
+
+        public static ShaderProgram LoadVSPSShader(string vs, string ps)
+        {
+            Shader vss = new Shader(ShaderType.VertexShader, ContentManager.Instance.Load<String>(vs));
+            Shader pss = new Shader(ShaderType.VertexShader, ContentManager.Instance.Load<String>(ps));
+
+            ShaderProgram program = new ShaderProgram();
+            program.AttachShader(vss);
+            program.AttachShader(pss);
+
+            return program;
+        }
+
+        public ShaderProgram()
+        {
+            Id = GL.CreateProgram();
+            Shaders = new List<Shader>();
+        }
+
+        ~ShaderProgram()
+        {
+            GL.DeleteProgram(Id);
+        }
+
+        public void AttachShader(Shader shader)
+        {
+            GL.AttachShader(Id, shader.Id);
+            Shaders.Add(shader);
+        }
+
+        public void Link()
+        {
+            GL.LinkProgram(Id);
+        }
+
+        public void Begin()
+        {
+            GL.UseProgram(Id);
+        }
+
+        public void End()
+        {
+            GL.UseProgram(0);
+        }
+    }
+}
